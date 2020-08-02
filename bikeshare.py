0569fcee82b679e00c4e429a73afd7beb8e32c68
Chigozie-Ezenwa <gozie.ezenwa@yahoo.co.uk>
2020-08-02 23:02:26 +0100

    refactor: Improve formatting for line comments

--- bikeshare.py
+++ bikeshare.py
@@ -16,8 +16,8 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington).
-    # HINT: Use a while loop to handle invalid inputs
+# TO DO: get user input for city (chicago, new york city, washington).
+# HINT: Use a while loop to handle invalid inputs
     
     input_city = input('Please choose one of these cities: Chicago, New York City, Washington \n').lower()
     while input_city not in ['chicago', 'new york city', 'washington']:
@@ -86,30 +86,30 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-#load file in dataframe
+# Load file in dataframe
 filename = ('{}.csv'.format(city))
 df = pd.read_csv(filename)
 
-#convert Start Time data to Date time
+# Convert Start Time data to Date time
 df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-#extract month and day of week from Start Time
+# Extract month and day of week from Start Time
 df['month'] = df['Start Time'].dt.month
 df['day_of_week'] = df['Start Time'].dt.weekday_name
 
-#filter by month if applicable
-#use index of months to get corresponding list
+# Filter by month if applicable
+# Use index of months to get corresponding list
 if month != 'all':
 months = ['January', 'February', 'March', 'April', 'May', 'June']
 month = months.index(month) + 1
 
-#filter by month to create new dataframe
+# Filter by month to create new dataframe
 df = df[df['month'] == month]
 
-#filter day of week if applicable
+# Filter day of week if applicable
 if day != 'all':
 
-#filter by day of week to create new dataframe
+# Filter by day of week to create new dataframe
 df = df[df['day_of_week'] == day.title()]
 
 return df
@@ -122,7 +122,7 @@ def time_stats(df):
 
 # TO DO: display the most common month
 
-#convert Start Time data to Date time
+# Convert Start Time data to Date time
 df['Start Time'] = pd.to_datetime(df['Start Time'])
 df['month'] = df['Start Time'].dt.month
 common_month = df['month'].mode()[0]
@@ -211,7 +211,7 @@ except:
      print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
     
-    # Displays raw data
+# TO DO: Displays raw data
 def raw_data(df):
     i = 0
     raw = ('\nWould you like to view the first 5 rows of raw data? Enter yes or no.\n').lower()
